c5af91ccd450fa79cb1f9711fdbd00d054debfe9
Isaac Oluwatemilorun <akiolisa@gmail.com>
2019-02-05 16:34:25 +0100

    Navigation route configuration. User live check for initial route added

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
 
+import 'package:jaacct/pages/home.dart';
 import 'package:jaacct/pages/login.dart';
+import 'package:jaacct/pages/signup.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+    @override
+    _MyApp createState() => _MyApp();
+}
+
+class _MyApp extends State<MyApp> {
+
+    bool _userLive = false;
+
     @override
     Widget build(BuildContext context) {
         return MaterialApp(
             title: 'Jochebed Apps',
-            home: Login(),
+            initialRoute: '/',
+            routes: {
+                '/': (BuildContext context) => _userLive ? Home() : Login(),
+                '/login': (BuildContext context) => Login(),
+                '/signup': (BuildContext context) => Signup()
+            },
+            // home: Login(),
         );
     }
 }
